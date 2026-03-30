e1171b66ca609a55bf36561e9b40678dda464e73
donli2035 <952987248@qq.com>
2026-03-30 18:03:43 +0800

    Create diagnosis page component

---
+++ app/diagnosis/page.tsx
@@ -0,0 +1,14 @@
+// This is a placeholder for the diagnosis page
+
+import React from 'react';
+
+const DiagnosisPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Diagnosis Page</h1>
+            <p>This is the diagnosis page of the application.</p>
+        </div>
+    );
+};
+
+export default DiagnosisPage;
