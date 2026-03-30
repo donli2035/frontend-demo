d63c8bf7f7b73e34053c05e32f1519afb87fbb80
donli2035 <952987248@qq.com>
2026-03-30 18:03:54 +0800

    Create upload page component

---
+++ app/upload/page.tsx
@@ -0,0 +1,13 @@
+// This is the page component for the upload route
+import React from 'react';
+
+const UploadPage = () => {
+    return (
+        <div>
+            <h1>Upload Page</h1>
+            {/* Upload functionality will go here */}
+        </div>
+    );
+};
+
+export default UploadPage;
